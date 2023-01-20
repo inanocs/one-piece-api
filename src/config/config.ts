2ea0eee139045acdf5e7c216815b08b87fd75225
inanocs <nanocabezasanchez@gmail.com>
2023-01-20 18:06:28 +0100

    fix: use nullish operator instead or

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 export const CONFIG = {
-  PORT: Number(process.env.PORT) || 3000,
-  NODE_ENV: process.env.NODE_ENV || 'dev',
-  HOST: process.env.HOST || 'localhost',
+  PORT: Number(process.env.PORT) ?? 3000,
+  NODE_ENV: process.env.NODE_ENV ?? 'dev',
+  HOST: process.env.HOST ?? 'localhost',
 }

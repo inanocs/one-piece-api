a74204c2f944c63760e987bebfbd3e543fb3c7ca
inanocs <nanocabezasanchez@gmail.com>
2023-01-20 18:13:34 +0100

    fix: linter options

--- src/server/server.config.logger.ts
+++ src/server/server.config.logger.ts
@@ -1,7 +1,7 @@
 import { PrettyOptions } from 'pino-pretty'
 import { PinoLoggerOptions } from 'fastify/types/logger.js'
 
-const options = {
+const options: PrettyOptions = {
   translateTime: 'yyyy-mm-dd HH:MM:ss Z',
   ignore: 'pid,hostname',
 }

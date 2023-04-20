97a6aa3449b620335b9a1709d720aa8c78bda0fb
inanocs <nanocabezasanchez@gmail.com>
2023-04-20 23:02:02 +0200

    feat: add yaml config

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,4 +1,11 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 8080,
-  env: process.env.NODE_ENV || 'local',
-});
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
+import { join } from 'path';
+
+const YAML_CONFIG_FILENAME = 'app.yaml';
+const SRC_PATH = join(__dirname, '..');
+export default () => {
+  return yaml.load(
+    readFileSync(join(SRC_PATH, YAML_CONFIG_FILENAME), 'utf8'),
+  ) as Record<string, any>;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './config/app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 8080);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('app.port');
+  const environment = configService.get<string>('app.node_env');
+  await app.listen(PORT, () => {
+    logger.log(
+      `Active environment: ${environment}, Listening on port: ${PORT}`,
+    );
+  });
 }
-bootstrap();
+void bootstrap();

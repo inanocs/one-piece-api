71c831dbd82e05efb79e24408f1cc8316e353c89
inanocs <nanocabezasanchez@gmail.com>
2023-05-19 22:59:18 +0200

    chore: add basic Sentry config setup

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/platform-fastify'
 import { Logger } from '@nestjs/common'
 import { HttpErrorFilter } from './shared/error/infra/rest/http-error.filter'
-
+import * as Sentry from '@sentry/node'
 async function bootstrap() {
   const logger = new Logger('main')
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -17,8 +17,14 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost)
   app.useGlobalFilters(new HttpErrorFilter(httpAdapter))
   const configService = app.get(ConfigService)
+  const SENTRY_HOST = configService.get<string>('app.sentry_host')
   const PORT = configService.get<number>('app.port')
   const environment = configService.get<string>('app.node_env')
+  Sentry.init({
+    dsn: SENTRY_HOST,
+    environment,
+    debug: true,
+  })
   await app.listen(PORT, '0.0.0.0')
   logger.log(`Active environment: ${environment}, Listening on port: ${PORT}`)
 }
